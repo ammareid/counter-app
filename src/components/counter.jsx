7e280f8f9e4075e71887cfe7b10e6c67608f0a85
Ammar Aleid <ammar.eid@gmail.com>
2022-06-06 15:39:23 +0300

    Input text handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,63 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: this.props.start,
+    tags: [],
+    tags1: ["tag1", "tag2", "tag3"],
+    num1: 1,
+    num2: 0,
+  };
+  // constructor() {
+  //   super();
+  //   this.handleChange = this.handleChange.bind(this);
+  // }
+
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>no tags</p>;
+    } else
+      return (
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      );
+  }
+
+  handleInputChange = (event, id) => {
+    var obj = {};
+    obj[event.target.id] = event.target.value;
+    this.setState(obj);
+
+    // this.setState({ [id]: event.target.value });
+    // console.log("event is: ", event);
+  };
+
+  handleClick = () => {
+    console.log(
+      "Button Click: num1: " + this.state.num1 + ", num2: " + this.state.num2
+    );
   };
 
   render() {
     let counterCssClasses = "badge m-2 ";
     counterCssClasses += this.state.count === 0 ? "bg-warning" : "bg-primary";
+
     return (
       <React.Fragment>
-        <span className={counterCssClasses}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <input
+          id="num1"
+          type="text"
+          value={this.state.num1}
+          onChange={this.handleInputChange}
+        />
+        <input
+          id="num2"
+          type="text"
+          value={this.state.num2}
+          onChange={this.handleInputChange}
+        />
+        <button onClick={this.handleClick}>Do</button>
       </React.Fragment>
     );
   }
